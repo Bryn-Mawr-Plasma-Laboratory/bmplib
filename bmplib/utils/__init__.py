a4b53c39a08e07f1b884aaadd921e79962f8c52c
cacsphysics <ccartagena@brynmawr.edu>
2020-10-28 17:20:46 -0500

    Created a utils package

---
+++ bmplib/utils/__init__.py
@@ -0,0 +1,14 @@
+#
+# This file is part of the bmplib package, a Python toolkit for the
+# Bryn Mawr Plasma Laboratory at Bryn Mawr College.
+#
+#
+#
+#
+
+"""
+The spectral analysis subpackage
+"""
+__all__ = ["index_funcs"]
+
+from bmplib.utils import index_funcs
